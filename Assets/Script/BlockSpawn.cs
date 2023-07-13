fc1a8f62967a071f99025f4f0594393c453b1047
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2023-07-13 14:05:58 +0900

    オブジェクトをカメラで表示

--- Assets/Script/BlockSpawn.cs
+++ Assets/Script/BlockSpawn.cs
@@ -5,8 +5,7 @@ using UnityEngine.UI;
 
 public class BlockSpawn : MonoBehaviour
 {
-    [SerializeField] float _interval = 1f;
-    [SerializeField] Text _text1;
+    [SerializeField] float _interval = 1f; 
     [SerializeField] Text _text2;
     [SerializeField] GameObject[] _objectBlock;
     //[SerializeField] GameObject _objectBlockH;
@@ -14,27 +13,29 @@ public class BlockSpawn : MonoBehaviour
     //[SerializeField] GameObject _objectBlockB;
     //[SerializeField] GameObject _objectCircle;
     //[SerializeField] GameObject _objectTriangle;
+    [SerializeField] GameObject _secondCamera;
     int _objectCount = 0;
     int _clickCount;
     float _timer;
     private Vector3 clickPosition;
     bool _swich = false;
-    
+    GameObject obj;
     // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_objectCount == 6)
-        {
-            _objectCount = 0;
-            _text2.text = _objectCount.ToString();
-        }
-        _timer += Time.deltaTime;
+        //if (_objectCount == 6)
+        //{-
+        //    _objectCount = 0;
+        //    
+        //}
+        _text2.text = (_objectCount % _objectBlock.Length).ToString();
+        _timer += Time.deltaTime; //左クリックを連打できないようにintervalをつけている
         if (_timer > _interval)
         {
             _swich = true;
@@ -45,43 +46,50 @@ public class BlockSpawn : MonoBehaviour
             if (_swich)
             {
                 _clickCount++;
+                _text2.text = _clickCount.ToString("");
                 clickPosition = Input.mousePosition;
                 clickPosition.z = 10f;
-                if (_objectCount == 0)
-                {
-                    Instantiate(_objectBlock[0], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[0].transform.rotation);
-                }
-                else if (_objectCount == 1)
-                {
-                    Instantiate(_objectBlock[1], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[1].transform.rotation);
-                }
-                else if (_objectCount == 2)
-                {
-                    Instantiate(_objectBlock[2], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[2].transform.rotation);
-                }
-                else if (_objectCount == 3)
-                {
-                    Instantiate(_objectBlock[3], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[3].transform.rotation);
-                }
-                else if (_objectCount == 4)
-                {
-                    Instantiate(_objectBlock[4], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[4].transform.rotation);
-                }
-                else if (_objectCount == 5)
-                {
-                    Instantiate(_objectBlock[5], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[5].transform.rotation);
-                }
+                //マウスカーソルの位置にオブジェクトが生成される
+                Instantiate(_objectBlock[_objectCount % _objectBlock.Length], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[_objectCount % _objectBlock.Length].transform.rotation);
+                //if (_objectCount == 0)
+                //{
+                //    Instantiate(_objectBlock[0], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[0].transform.rotation);
+                //    //Sprite sprite = 
+                //}
+                //else if (_objectCount == 1)
+                //{
+                //    Instantiate(_objectBlock[1], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[1].transform.rotation);
+                //}
+                //else if (_objectCount == 2)
+                //{
+                //    Instantiate(_objectBlock[2], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[2].transform.rotation);
+                //}
+                //else if (_objectCount == 3)
+                //{
+                //    Instantiate(_objectBlock[3], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[3].transform.rotation);
+                //}
+                //else if (_objectCount == 4)
+                //{
+                //    Instantiate(_objectBlock[4], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[4].transform.rotation);
+                //}
+                //else if (_objectCount == 5)
+                //{
+                //    Instantiate(_objectBlock[5], Camera.main.ScreenToWorldPoint(clickPosition), _objectBlock[5].transform.rotation);
+                //}
                 _swich = false;
-                _text1.text = _clickCount.ToString();
-
+                if (obj != null)
+                {
+                    Destroy(obj);
+                }
+                //別のカメラで今出ているオブジェクトを撮影している
+                obj = Instantiate(_objectBlock[_objectCount % _objectBlock.Length], _secondCamera.transform.position + new Vector3(0, 0, 10f), Quaternion.identity, _secondCamera.transform);
+                obj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
             }
         }
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1)) //左クリックで出るオブジェクトを右クリックで変えている
         {
             Debug.Log(_objectCount);
             _objectCount++;
-            _text2.text = _objectCount.ToString("");
-
         }
     }
 }
