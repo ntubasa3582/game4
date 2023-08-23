82ff05e21ed0d42c667b707a55bb41a238fff62f
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2023-08-23 12:17:38 +0900

    ブロック設置オフセットの動的初期化ができるようになった

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -7,13 +7,20 @@ public class BallController : MonoBehaviour
     private Rigidbody2D rigidbody2D;
     [Header("X座標")] 
     [SerializeField, Tooltip("X座標")] float _xpos;
-
     [Header("Y座標")]
     [SerializeField, Tooltip("Y座標")] float _ypos;
-
+    //コライダーのオフセット
+    const float _offset = 1.5f;
+    //このオブジェクトの子オブジェクト
+    GameObject _goChild;
+    //子オブジェクトのCIrcleCollider2D
+    CircleCollider2D _cC2D;
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        _goChild = this.transform.GetChild(0).gameObject;
+        _cC2D = _goChild.GetComponent<CircleCollider2D>();
+        _cC2D.radius = _offset;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -23,7 +30,6 @@ public class BallController : MonoBehaviour
             transform.position = new Vector2(_xpos, _ypos); 
             rigidbody2D.drag += 3;
             StartCoroutine("DragChange");
-
         }
     }
     IEnumerator DragChange()
