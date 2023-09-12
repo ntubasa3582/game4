16ce4c0b847a3d4773c4fb3c968571e52afdd7d2
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2023-09-12 14:08:18 +0900

    SceneをまたいでAudioを再生できるようにする

---
+++ Assets/Script/TestScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject _obj1;
+    //[SerializeField] GameObject _obj2;
+    void Start()
+    {
+
+        if (FindObjectsOfType<TestScript>().Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(_obj1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
